392b3d0c00c866b161add7f38418530c256a2384
Noel George <noelgeorge2004@gmail.com>
2023-05-23 01:00:38 +0530

    Finished task addition script

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 /* Add task buttons */
 const addTask = document.querySelectorAll('[data-modal-target]');
-let taskForm = document.querySelector('aside')
+let taskFormContainer = document.querySelector('aside')
 let overlay = document.querySelector('.overlay');
-let taskCloseButton = document.getElementById('form-close-button')
+let taskCloseButton = document.getElementById('form-close-button');
+
+let taskForm = document.getElementById("task-form");
+
+let todo = document.getElementById("to-do");
+let progress = document.getElementById("in-progress");
+let completed = document.getElementById("completed");
+
+let activeTaskArea = todo;
 
 addTask.forEach(taskButton => {
     taskButton.addEventListener("click", () => {
-        taskForm.classList.add('active');
+        taskFormContainer.classList.add('active');
         overlay.classList.add('active');
+
+        if (taskButton.classList.contains("todo")) {
+            activeTaskArea = todo;
+        }
+
+        else if (taskButton.classList.contains("progress")) {
+            activeTaskArea = progress;
+        }
+
+        else if (taskButton.classList.contains("completed")) {
+            activeTaskArea = completed;
+        }
     })
 })
 
 taskCloseButton.addEventListener("click", () => {
-    taskForm.classList.remove('active');
+    taskFormContainer.classList.remove('active');
     overlay.classList.remove('active');
 })
+
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let taskName = document.getElementById("task-name-input");
+    let taskDescr = document.getElementById("task-desc-input");
+
+    let task = document.createElement("div");
+    task.classList.add("task");
+
+    console.log(taskName, taskDescr);
+    
+    let taskNameContainer = document.createElement("div");
+    taskNameContainer.classList.add("task-name");
+    taskNameContainer.innerText = taskName.value;
+
+    let taskDescrContainer = document.createElement("div");
+    taskDescrContainer.classList.add("description");
+    taskDescrContainer.innerText = taskDescr.value;
+
+    task.appendChild(taskNameContainer);
+    task.appendChild(taskDescrContainer);
+
+    activeTaskArea.appendChild(task);
+
+    /* Remove form after submitting */
+    taskName.value = "";
+    taskDescr.value = "";
+    taskFormContainer.classList.remove('active');
+    overlay.classList.remove('active');
+})
+
